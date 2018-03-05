a5ff0955d7a6b5aebd08e0ff1983cfcae58c362d
Yoon Sung <yoonsung0711@gmail.com>
2018-03-05 14:19:23 +0900

    81p/lists/tests.py

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -54,5 +54,12 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, '신규 작업 아이템')
 
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
