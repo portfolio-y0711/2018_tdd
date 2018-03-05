028a2af2464112573a3d27f91da1bc3ea23742e1
Yoon Sung <yoonsung0711@gmail.com>
2018-03-05 09:49:45 +0900

    61p/functional_tests.py

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -36,6 +36,8 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: 공작깃털 사기" 아이템이 추가된다
         inputbox.send_keys(Keys.ENTER)
 
+        import time
+        time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
