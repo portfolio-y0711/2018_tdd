a4ad6ed51572b8b5b4a4f4aba62026ff5186ce52
Yoon Sung <yoonsung0711@gmail.com>
2018-03-05 11:22:31 +0900

    71p/lists/tests.py

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,8 +3,27 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 
+from lists.models import Item
 from lists.views import home_page
 
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        first_item = Item()
+        first_item.text = '두 번째 아이템'
+        first_item.save()
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
 
 class HomePageTest(TestCase):
 
