572c8b6b168499969291da2ef3f594b8da015279
Yoon Sung <yoonsung0711@gmail.com>
2018-03-06 09:10:02 +0900

    110p/lists/views.py(3)

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from lists.models import Item
 
 def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
     return redirect('/lists/the-only-list-in-the-world/') 
 
 def view_list(request):
