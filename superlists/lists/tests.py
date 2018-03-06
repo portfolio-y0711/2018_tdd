4466eae549df0dcb843bce49c49ee878c4e096aa
Yoon Sung <yoonsung0711@gmail.com>
2018-03-06 13:06:22 +0900

    122p/lists/tests.py

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,6 +6,33 @@ from django.http import HttpRequest
 from lists.models import Item, List
 from lists.views import home_page
 
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': '기존 목록에 신규 아이템'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '기존 목록에 신규 아이템'),
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': '기존 목록에 신규 아이템'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
 class NewListTest(TestCase):
 
     def test_saving_a_POST_request(self):
