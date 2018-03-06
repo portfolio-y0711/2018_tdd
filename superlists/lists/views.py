cb43acb9a336903098da2ed1eaa9032111e27c5a
Yoon Sung <yoonsung0711@gmail.com>
2018-03-06 15:30:56 +0900

    125p/lists/views.py

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,6 +3,7 @@ from lists.models import Item, List
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
 
 def new_list(request):
