acc9eff3eee924a1fc35a7b9769fc27b1534ae09
Yoon Sung <yoonsung0711@gmail.com>
2018-03-06 10:34:08 +0900

    116p/delete lists/migration/0004_item_list.py & python manage.py makemigrations

--- superlists/lists/migrations/0004_item_list.py
+++ superlists/lists/migrations/0004_item_list.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='item',
             name='list',
-            field=models.TextField(default=''),
+            field=models.ForeignKey(to='lists.List', default=None),
             preserve_default=True,
         ),
     ]
