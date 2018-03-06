07a91039958f1b6342fe24c3fe7048084f339c7c
Yoon Sung <yoonsung0711@gmail.com>
2018-03-06 09:57:30 +0900

    113p/lists/tests.py#second->first로 오기한 부분(71p)도 바로잡음

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 class NewListTest(TestCase):
@@ -39,24 +39,34 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = '첫 번째 아이템'
-        first_item.save()
+        list_ = List()
+        list_.save()
 
         first_item = Item()
-        first_item.text = '두 번째 아이템'
+        first_item.text = '첫 번째 아이템'
+        first_item.list = list_
         first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.list = list_
+        second_item.save()
         
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
 
 class HomePageTest(TestCase):
 
