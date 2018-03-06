e96a86cca5e911f57f2a4f519d41676b54a3f002
Yoon Sung <yoonsung0711@gmail.com>
2018-03-06 09:28:02 +0900

    111p/lists/tests.py

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,7 +8,7 @@ from lists.views import home_page
 
 class NewListTest(TestCase):
 
-    def test_home_page_can_save_a_POST_request(self):
+    def test_saving_a_POST_request(self):
         self.client.post(
             '/lists/new',
             data={'item_text': '신규 작업 아이템'}
@@ -17,13 +17,12 @@ class NewListTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, '신규 작업 아이템')
 
-    def test_home_page_redirects_after_POST(self):
+    def test_redirects_after_POST(self):
         response =self.client.post(
             '/lists/new',
             data={'item_text': '신규 작업 아이템'}
         )
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 class ListViewTest(TestCase):
 
