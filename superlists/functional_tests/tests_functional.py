113aca3982d57fc867e9b545f3c5f5a578595891
lunxiang0711 <lunxiang0711@gmail.com>
2018-03-14 11:35:47 +0900

    #delta 값 조정

--- superlists/functional_tests/tests_functional.py
+++ superlists/functional_tests/tests_functional.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         #에디스는 메인 페이지를 방문한다
@@ -14,7 +14,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=300
         )
 
         # 그녀는 새로운 리스트를 시작하고 입력 상자가
@@ -24,7 +24,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta = 10
+            delta = 300
         )
     def setUp(self):
         self.browser = webdriver.Chrome()
