7afb1b1fdbae7023735f68fb56d6a0e47c98ca96
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-04-30 16:51:12 +0900

    210p/lists/views.py

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from lists.models import Item, List
 
+def new_list(request):
+    list_ = List.objects.create()
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item.full_clean()
+    return redirect('/lists/%d/' % (list_.id,))
+
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
-
-def new_list(request):
-    list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,)) 
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
