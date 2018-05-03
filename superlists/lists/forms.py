f01ecb1b97bc1008ef7d31a537ebee59d16de185
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-05-03 15:14:11 +0900

    227p/lists/forms.py

--- superlists/lists/forms.py
+++ superlists/lists/forms.py
@@ -4,6 +4,7 @@ class ItemForm(forms.Form):
     item_text = forms.CharField(
         widget=forms.fields.TextInput(attrs={
             'placeholder': '작업 아이템 입력',
+            'class': 'form-control input-lg'
         }),
     )
     
