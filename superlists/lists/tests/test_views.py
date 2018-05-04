699b18d255d69736a8e3161b3d6db1f7785e5124
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-05-04 09:52:16 +0900

    238p/lists/tests/test_views.py

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -6,7 +6,7 @@ from django.http import HttpRequest
 
 from lists.models import Item, List
 from lists.views import home_page
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
 
 class NewListTest(TestCase):
 
@@ -27,28 +27,26 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
+    def test_for_invalid_input_renders_home_template(self):
         response = self.client.post('/lists/new', data={'text': ''})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
 
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_validation_errors_end_up_on_lists_page(self):
-        list_ = List.objects.create()
+    def test_validation_errors_are_shown_on_home_page(self):
         response = self.client.post(
-            '/lists/%d/' % (list_.id,),
+            '/lists/new',
             data={'text': ''}
         )
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'list.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+        self.assertContains(response, escape(EMPTY_LIST_ERROR))
 
 class ListViewTest(TestCase):
 
