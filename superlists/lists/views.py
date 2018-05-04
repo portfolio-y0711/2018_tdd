fd72b09b08a59b48da57ca9b2498e91d537a75aa
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-05-04 11:51:34 +0900

    241p/lists/views.py

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -25,7 +25,8 @@ def view_list(request, list_id):
         except ValidationError:
             error = "You can't have an empty list item"
 
-    return render(request, 'list.html', {'list': list_, 'error': error})
+    form = ItemForm()
+    return render(request, 'list.html', {'list': list_, "form": form, 'error': error})
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
