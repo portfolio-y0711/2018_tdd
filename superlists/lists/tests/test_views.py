63fee90bc303604a6e51820bff42ef517a5469c0
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-05-08 12:00:40 +0900

    258p/lists/tests/test_views.py

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -117,6 +117,15 @@ class ListViewTest(TestCase):
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post('/lists/%d/' % (list1.id,), data={'text': 'textey'})
+
+        expected_error = escape("이미 리스트에 해당 아이템이 있습니다")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class HomePageTest(TestCase):
